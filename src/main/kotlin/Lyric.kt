cc8d11f2012d61b6698747ba3a96e965197b3b4a
nsa <nsa@technokad.ru>
2022-09-16 20:20:39 +0300

    Initial commit

--- src/main/kotlin/Lyric.kt
+++ src/main/kotlin/Lyric.kt
@@ -1,5 +1,6 @@
 import java.io.File
 import java.lang.Long.max
+import java.lang.Long.min
 
 class Lyric {
     var items: List<LyricLine> = emptyList()
@@ -7,6 +8,7 @@ class Lyric {
         fun getLiric(subtitles: Subtitles): Lyric {
 
             val fileName = "src/main/resources/lyrics.txt"
+            val fileNameKdeTitile = "src/main/resources/lyrics.kdenlivetitle"
             var startLine: String? = null
             var endLine: String? = null
 
@@ -70,20 +72,62 @@ class Lyric {
                         )
                         currentPositionEnd += silentLineDuration
                         result.add(liricEmpty)
-                        text += "[$startDuration --> $endDuration]: \n"
+//                        text += "[$startDuration --> $endDuration]: \n"
+                        text += "\n"
                     }
                 }
                 result.add(liric)
                 currentPositionEnd = convertTimecodeToMilliseconds(liric.end!!)
-                text += "[${liric.start} --> ${liric.end}]: ${liric.text}\n"
+//                text += "[${liric.start} --> ${liric.end}]: ${liric.text}\n"
+                text += "${liric.text}\n"
             }
 
             println(text)
 
-            File(fileName).writeText(text)
-
             var resultLyric = Lyric()
             resultLyric.items = result
+
+            val maxTextWidth = FRAME_WIDTH - KLT_ITEM_CONTENT_TITLE_POSITION_START_X * 2
+            val maxTextLength = resultLyric.items.maxBy { it.text!!.length }.text!!.length
+            val maxSymbolWidth = maxTextWidth / maxTextLength
+            val maxFontSize = min((maxSymbolWidth * KLT_ITEM_CONTENT_FONT_PIXEL_SIZE) / KLT_ITEM_CONTENT_FONT_SYMBOL_WIDTH, KLT_ITEM_CONTENT_FONT_PIXEL_SIZE)
+            val currentFontSymbolHeight = maxFontSize * KLT_ITEM_CONTENT_FONT_SYMBOL_HEIGHT / KLT_ITEM_CONTENT_FONT_PIXEL_SIZE
+            val currentFontSymbolWidth = maxFontSize * KLT_ITEM_CONTENT_FONT_SYMBOL_WIDTH / KLT_ITEM_CONTENT_FONT_PIXEL_SIZE
+
+            val boxHeight = resultLyric.items.size * currentFontSymbolHeight
+            val boxWidth = maxTextLength * currentFontSymbolWidth
+
+            val template = """
+<kdenlivetitle duration="0" LC_NUMERIC="C" width="$FRAME_WIDTH" height="$boxHeight" out="0">
+ <item type="QGraphicsTextItem" z-index="0">
+  <position x="$KLT_ITEM_CONTENT_TITLE_POSITION_START_X" y="$KLT_ITEM_CONTENT_TITLE_POSITION_START_Y">
+   <transform>1,0,0,0,1,0,0,0,1</transform>
+  </position>
+  <content 
+        line-spacing="$KLT_ITEM_CONTENT_LINE_SPACING" 
+        shadow="$KLT_ITEM_CONTENT_SHADOW" 
+        font-underline="$KLT_ITEM_CONTENT_FONT_UNDERLINE" 
+        box-height="$boxHeight" 
+        font="$KLT_ITEM_CONTENT_FONT_NAME" 
+        letter-spacing="0" 
+        font-pixel-size="$maxFontSize" 
+        font-italic="$KLT_ITEM_CONTENT_FONT_ITALIC" 
+        typewriter="$KLT_ITEM_CONTENT_TYPEWRITER" 
+        alignment="$KLT_ITEM_CONTENT_ALIGNMENT" 
+        font-weight="$KLT_ITEM_CONTENT_FONT_WEIGHT" 
+        box-width="$boxWidth" 
+        font-color="$KLT_ITEM_CONTENT_FONT_COLOR"
+        >$text<
+        /content>
+ </item>
+ <startviewport rect="0,0,$FRAME_WIDTH,$boxHeight"/>
+ <endviewport rect="0,0,$FRAME_WIDTH,$boxHeight"/>
+ <background color="0,0,0,0"/>
+</kdenlivetitle>"""
+
+
+            File(fileName).writeText(text)
+            File(fileNameKdeTitile).writeText(template)
 
             return resultLyric
         }

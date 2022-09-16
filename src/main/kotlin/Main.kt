9bdebe3a4e2ecdfd770453a97281a837a14d5da4
nsa <nsa@technokad.ru>
2022-09-16 18:45:12 +0300

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,4 @@ fun main(args: Array<String>) {
 
     Liric.createLiric(Subtitles.getSubtitles())
 
-    println(convertTimecodeToMilliseconds("00:00:39,133"))
-    println(convertMillisecondsToTimecode(39133))
-    println(getDurationInMilliseconds("00:00:39,833", "00:00:40,116"))
-    println(convertMillisecondsToTimecode(getDurationInMilliseconds("00:00:39,833", "00:00:40,116")))
 }

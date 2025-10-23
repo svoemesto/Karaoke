f78e99be5f1cf7eee2a980a325773aec3d1b4c8d
nsa <nsa-svoemesto@yandex.ru>
2025-10-23 18:50:27 +0300

    Текущие изменения по состоянию на 23.10.2025 + рефактор ApiController.kt

--- karaoke-app/src/main/kotlin/com/svoemesto/karaokeapp/config/SecurityConfig.kt
+++ karaoke-app/src/main/kotlin/com/svoemesto/karaokeapp/config/SecurityConfig.kt
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+
 // УБРАНО: import org.springframework.security.oauth2.jwt.JwtDecoder // Удалено: не нужно для открытого /api/**
 // УБРАНО: import org.springframework.security.web.util.matcher.AntPathRequestMatcher // Удалено: не нужно
 

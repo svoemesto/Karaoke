136cd94254fcc8f94617ffc66c8d82a3afe8813d
Ilya (boss) <boss@karaoke.local>
2026-09-15 15:16:37 +0300

    ProcessWorker.vue: periodic poll cacheQueueSize каждые 5с (specs/118 #397)
    Владелец сообщил, что бейдж показывает '0' даже когда задачи есть. Возможно,
    initial poll в mounted() не срабатывает или ответ не приходит вовремя.
    
    Добавлен setInterval(5000) в mounted() + clearInterval в beforeUnmount(),
    который вызывает checkCacheQueueSize() каждые 5 секунд. Это гарантирует,
    что бейдж обновляется регулярно, даже если initial poll не сработал.
    
    Также добавлен clearInterval в beforeUnmount() чтобы не утекал интервал
    при переключении компонента.
    
    Refs: PR #494-#498, OpenProject #118.

--- webvue3/src/components/Common/ProcessWorker.vue
+++ webvue3/src/components/Common/ProcessWorker.vue
@@ -79,6 +79,8 @@ export default {
     return {
       isConfirmVisible: false,
       confirmParams: undefined,
+      // specs/118 #397: interval для periodic poll размера cache-очереди.
+      cacheQueueSizeInterval: null,
     }
   },
   computed: {
@@ -137,8 +139,16 @@ export default {
   mounted() {
     this.checkUpdateProcessesWorker()
     this.checkCountWaiting()
-    // specs/118 #397: initial poll размера cache-очереди.
+    // specs/118 #397: initial poll + periodic poll каждые 5с для размера cache-очереди.
     this.checkCacheQueueSize()
+    this.cacheQueueSizeInterval = setInterval(() => this.checkCacheQueueSize(), 5000)
+  },
+  beforeUnmount() {
+    // specs/118 #397: очистить interval при unmount.
+    if (this.cacheQueueSizeInterval) {
+      clearInterval(this.cacheQueueSizeInterval)
+      this.cacheQueueSizeInterval = null
+    }
   },
   methods: {
     clickStartStopWorkerButton() {
